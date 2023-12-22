e9cc31233f126ab4c05c39087c96f79d73f83925
cpackard <6462409+cpackard@users.noreply.github.com>
2023-12-22 12:09:42 -0500

    add hooks for better golden-ratio-mode

--- config.el
+++ config.el
@@ -219,3 +219,14 @@
   (setq lsp-inlay-hint-enable t)
   (setq lsp-ui-peek-always-show t)
   (setq lsp-ui-sideline-show-hover t))
+
+;; Golden Ratio
+;; https://github.com/doomemacs/doomemacs/issues/2225#issuecomment-568287835
+(use-package! golden-ratio
+  :after-call pre-command-hook
+  :config
+  (golden-ratio-mode 1)
+  ;; Using this hook for resizing windows is less precise than
+  ;; `doom-switch-window-hook'.
+  (remove-hook 'window-configuration-change-hook #'golden-ratio)
+  (add-hook 'doom-switch-window-hook #'golden-ratio))
