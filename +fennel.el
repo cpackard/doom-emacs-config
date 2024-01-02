d18700a2ba7a9deaf9fade3a26aa3a38cf24a5fb
cpackard <6462409+cpackard@users.noreply.github.com>
2024-01-02 07:14:41 -0500

    register fennel LSP mode

--- +fennel.el
+++ +fennel.el
@@ -9,4 +9,16 @@
         :localleader
         :prefix ("r" . "repl")
         :desc "base repl" "s" #'fennel-repl
+        :desc "proto repl" "p" #'fennel-proto-repl
         :desc "LÖVE repl" "l" #'fennel-love-2d-repl))
+
+(after! (:and lsp-mode fennel-mode)
+  (add-to-list 'lsp-language-id-configuration
+               '(fennel-mode . "fennel"))
+
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection "fennel-ls")
+                    :activation-fn (lsp-activate-on "fennel")
+                    :server-id 'fennel-ls))
+
+  (add-hook 'fennel-mode-hook #'lsp))
