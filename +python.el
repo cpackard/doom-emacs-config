76a4773090eaec311c12ca779fc97671fe0ab4ef
Christian Packard <6462409+cpackard@users.noreply.github.com>
2024-02-12 05:19:28 -0500

    add sort-import-on-save python hook

--- +python.el
+++ +python.el
@@ -15,18 +15,18 @@
                                      :django t))
 
   (map! :map python-mode-map
-      :localleader
-      :prefix ("d" . "DAP")
-      :desc "debug" "d" #'dap-debug
-      :desc "continue" "c" #'dap-continue
-      :desc "step in" "i" #'dap-step-in
-      :desc "step out" "o" #'dap-step-out
-      :desc "step over" "n" #'dap-next
-      :desc "toggle breakpoint" "b" #'dap-breakpoint-toggle
-      :desc "evaluate expression" "e" #'dap-eval-thing-at-point
-      :desc "restart" "r" #'dap-debug-restart
-      :desc "disconnect" "q" #'dap-disconnect
-      :desc "close" "Q" #'dap-delete-all-sessions))
+        :localleader
+        :prefix ("d" . "DAP")
+        :desc "debug" "d" #'dap-debug
+        :desc "continue" "c" #'dap-continue
+        :desc "step in" "i" #'dap-step-in
+        :desc "step out" "o" #'dap-step-out
+        :desc "step over" "n" #'dap-next
+        :desc "toggle breakpoint" "b" #'dap-breakpoint-toggle
+        :desc "evaluate expression" "e" #'dap-eval-thing-at-point
+        :desc "restart" "r" #'dap-debug-restart
+        :desc "disconnect" "q" #'dap-disconnect
+        :desc "close" "Q" #'dap-delete-all-sessions))
 
 (defun python-pytest-all ()
   (interactive)
@@ -56,4 +56,8 @@
         :desc "send buffer" "b" #'python-shell-send-buffer
         :desc "send function" "f" #'python-shell-send-defun
         :desc "send region" "r" #'python-shell-send-region
-        :desc "send statement" "s" #'python-shell-send-statement))
+        :desc "send statement" "s" #'python-shell-send-statement)
+  (add-hook! '(before-save-hook)
+    (defun +python--isort-buffer ()
+      (when (eq major-mode 'python-mode)
+        (call-interactively 'py-isort-buffer)))))
