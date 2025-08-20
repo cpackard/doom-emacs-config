3bcc596fd0dc651109d4ae63f8f7cc32ba165228
cpackard <6462409+cpackard@users.noreply.github.com>
2025-08-20 13:53:21 -0400

    add +pyright to python config

--- init.el
+++ init.el
@@ -157,7 +157,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +poetry +pyenv +tree-sitter)            ; beautiful is better than ugly
+       (python +lsp +poetry +pyenv +tree-sitter +pyright)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
